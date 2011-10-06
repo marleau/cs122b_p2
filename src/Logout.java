5a096e2a86eabde451933b242ed388b6b8ec7723
TheNeisius <abrageddon@gmail.com>
2011-10-06 09:31:52 -0700

    forgot to add logout page to git

---
+++ src/Logout.java
@@ -0,0 +1,39 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Logout() {
+        super();
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		session.removeAttribute("user.login");
+		response.sendRedirect("/Fabflix/");
+	}
+
+	public static void button(PrintWriter out) {
+		out.println("<a href=\"Logout\">Log Out</a>");
+		
+	}
+
+}
